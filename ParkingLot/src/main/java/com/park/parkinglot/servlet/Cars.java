a30184dca4c0bf6b203ab8343b0fafe6e9e78cbb
BigBoiAlex <realbigboialex@gmail.com>
2021-11-11 13:57:09 +0200

    Partea 7 - userpage

--- ParkingLot/src/main/java/com/park/parkinglot/servlet/Cars.java
+++ ParkingLot/src/main/java/com/park/parkinglot/servlet/Cars.java
@@ -4,8 +4,12 @@
  */
 package com.park.parkinglot.servlet;
 
+import com.park.parkinglot.common.CarDetails;
+import com.park.parkinglot.ejb.CarBean;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +22,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "Cars", urlPatterns = {"/Cars"})
 public class Cars extends HttpServlet {
+
+    @Inject
+    private CarBean carBean;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -58,7 +65,12 @@ public class Cars extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //processRequest(request, response);
+        request.setAttribute("activePage", "Cars");
         request.setAttribute("numberOfFreeParkingSpots", 10);
+
+        List<CarDetails> cars = carBean.getAllCars();
+        request.setAttribute("cars", cars);
+
         request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request, response);
     }
 
